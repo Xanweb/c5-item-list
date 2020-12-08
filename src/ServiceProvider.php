d1e5fb323bbc1a9c8c01c10865e7b1cafca56e22
Hamed Darragi <hd@xanweb.com>
2020-12-08 15:28:00 +0100

    Update library according to latest changes made under xanweb/c5-foundation v1.3

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,20 +1,63 @@
 <?php
+
 namespace Xanweb\ItemList;
 
-use Concrete\Core\Asset\AssetList;
 use Concrete\Core\Foundation\Service\Provider as CoreServiceProvider;
-use Concrete\Core\Support\Facade\Route;
 use Xanweb\ExtAsset\Asset\VendorAssetManager;
-use Xanweb\ItemList\Route\RouteList;
+use Xanweb\Foundation\Config\BeforeRenderDefaultAssetJS;
+use Xanweb\Foundation\Config\JavascriptAssetDefaults;
 
 class ServiceProvider extends CoreServiceProvider
 {
     public function register(): void
     {
-        $router = Route::getFacadeRoot();
-        $router->loadRouteList($this->app->build(RouteList::class));
-
+        $this->registerListeners();
         $this->registerAssets();
+    }
+
+    protected function registerListeners(): void
+    {
+        $this->app['director']->addListener(BeforeRenderDefaultAssetJS::NAME, function (BeforeRenderDefaultAssetJS $event) {
+            JavascriptAssetDefaults::macro('mergeWith', function (array $items) {
+                $this->items = array_merge($this->items, $items);
+            });
+
+            $event->getJavascriptAssetDefaults()->mergeWith([
+                'i18n' => [
+                  'confirm' => t('Are you sure?'),
+                  'maxItemsExceeded' => t('Max items exceeded, You can not add any more items.'),
+                  'pageNotFound' => t('Page not found'),
+                ],
+                'editor' => [
+                  'initRichTextEditor' => $this->getInitRichTextEditorJSFunction(),
+                  'destroyRichTextEditor' => $this->getDestroyRichTextEditorJSFunction(),
+                ],
+            ]);
+        });
+    }
+
+    protected function getInitRichTextEditorJSFunction()
+    {
+        return $this->app['config']->get('xanweb.item_list.editor.initRichTextEditorJSFunc', function () {
+            return $this->app['editor']->getEditorInitJSFunction();
+        });
+    }
+
+    protected function getDestroyRichTextEditorJSFunction()
+    {
+        return $this->app['config']->get('xanweb.item_list.editor.destroyRichTextEditorJSFunc', function () {
+            return <<<EOT
+function (editor) {
+    var id = editor.attr('id');
+    if (CKEDITOR.instances[id] !== undefined) {
+        CKEDITOR.instances[id].destroy();
+    }
+    
+    editor.remove();
+    $('#cke_' + id).remove();
+}
+EOT;
+        });
     }
 
     protected function registerAssets(): void
@@ -26,15 +69,12 @@ class ServiceProvider extends CoreServiceProvider
             ],
         ]);
 
-        $al = AssetList::getInstance();
-        $al->register('javascript-localized', 'xw/item-list/defaults', '/xw/item-list/js/defaults.js');
-
         VendorAssetManager::registerGroupMultiple([
             'xw/item-list' => [
                 [
                     ['javascript', 'jquery'],
                     ['javascript', 'underscore'],
-                    ['javascript-localized', 'xw/item-list/defaults'],
+                    ['javascript-localized', 'xw/defaults'],
                     ['vendor-javascript', 'xw/item-list'],
                     ['vendor-css', 'xw/item-list'],
                 ],
